bbffa8accc956c39f9e261e462eef439b9d123ba
MightyOtter <127059377+MightyOtter@users.noreply.github.com>
2023-05-18 16:41:34 +0100

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,29 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+rem Set the directory path where the input files are located
+set "input_path=E:\Github\tortoise-tts\Inputfiles"
+
+rem Set the directory path where the output files should be saved
+set "output_dir=E:\Github\tortoise-tts\results"
+
+rem Create the output directory if it doesn't already exist
+if not exist "%output_dir%" (
+  mkdir "%output_dir%"
+)
+
+rem Loop through all the .txt files in the input directory
+for %%i in ("%input_path%\*.txt") do (
+
+  rem Get the file name without extension
+  set "file_name=%%~ni"
+  
+  rem Set the output directory path with the file name
+  set "output_path=%output_dir%\!file_name!"
+  
+  rem Run the Python command with the current input file and output directory
+  python tortoise/read.py --voice stephenfry --text "%%~i" --preset ultra_fast --output_path "!output_path!"
+)
+
+rem Pause the script so that it doesn't immediately close when finished
+pause
